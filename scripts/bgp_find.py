d2426db0f8f3aee7177147ca563fc10cd1fb2686
Cesar Vargas <vargasces@gmail.com>
2012-09-14 15:28:43 -0400

    Basic searching work

--- scripts/bgp_find.py
+++ scripts/bgp_find.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-print 'hello'
+import re
+response = raw_input("Enter the prefix you are looking for: ")
+sentinel = 0
+pattern = re.compile ("PREFIX: " + response)
 f = open('rib.20041201.0046.txt','r')
 for line in f:
-    print line,
+    if pattern.match(line):
+        sentinel = sentinel + 1
+        print line,
 f.close()
+print ("Total instances of " + response)
+print sentinel
