0c0b25539b4bab4aa83ac26606d3bf8fe041df89
Cesar Vargas <vargasces@gmail.com>
2012-09-26 14:31:46 -0400

    seems to parse correctly and find as numbers that end with a specific as number

--- scripts/bgp_find.py
+++ scripts/bgp_find.py
@@ -2,7 +2,9 @@
 from itertools import islice
 import re
 response = raw_input("Enter the prefix you are looking for: ")
+myfilename = raw_input("Enter the filename to analyze: ")
 sentinel = 0
+sentinel2 = 0
 tmpattern = re.compile (r"(TIME:) (\w+/\w+/\w+ \w+:\w+:\w+)")
 tppattern = re.compile (r"(TYPE:) (\w+/\w+)")
 vipattern = re.compile (r"(VIEW:) (\w+)")
@@ -11,10 +13,11 @@ prepattern = re.compile (r"(PREFIX:) (\w+.\w+.\w+.\w+/\w)")
 frompattern = re.compile (r"(FROM:) (\w+.\w+.\w+.\w+ \w+)")
 origipattern = re.compile (r"(ORIGINATED:) (\w+/\w+/\w+ \w+:\w+:\w+)")
 originpattern = re.compile (r"(ORIGIN:) (\w+)")
-aspathpattern = re.compile (r"(ASPATH:) (\w+)")
+#aspathpattern = re.compile (r"(ASPATH:) (\w+)")
+aspathpattern = re.compile (r"ASPATH: ")
 nexthoppattern = re.compile (r"(NEXT_HOP:) (\w+.\w+.\w+.\w+)" )
 statspattern = re.compile (r"(STATUS:) (\w+)")
-f = open('rib.20041201.0046.txt','r')
+f = open(myfilename,'r')
 while True:
     next_12_lines = list(islice(f,12))
     if not next_12_lines:
@@ -40,14 +43,22 @@ while True:
             elif originpattern.match(line):
                 originp = originpattern.match(line).group(2)
             elif aspathpattern.match(line):
-                aspathp = aspathpattern.match(line).group(2)
+                tmp = aspathpattern.split(line)
+                #aspathp = aspathpattern.match(line).group(2)
+                aspathp = tmp[1]
             elif nexthoppattern.match(line): 
                 nexthop = nexthoppattern.match(line).group(2)
             elif statspattern.match(line):
                 statspath = statspattern.match(line).group(2)
                 
         if ( bool(tm) & bool(tp) & bool(vip) & bool(sep) & bool(prep) & bool(frompat) & bool(origip) & bool(originp) & bool(aspathp) & bool(nexthop) & bool(statspath) ):
-            if response == prep:
-                print "I am done with 12 lines and string we capture is ", tm, " ", tp, " ", vip, " ", sep, " ", prep, " ", frompat, " ", origip, " ", originp, " ", aspathp, " ", nexthop," ", statspath
+            sentinel = sentinel + 1
+#            if response == prep:
+#            if re.search('\\b'+response+'\\b',aspathp):
+            if re.search('\\b'+response+'$',aspathp):
+#                print tm, " ", tp, " ", vip, " ", sep, " ", prep, " ", frompat, " ", origip, " ", originp, " ", aspathp, " ", nexthop," ", statspath
+                sentinel2 = sentinel2 + 1
 
 f.close()
+print "Total number of entries: ", sentinel
+print "Total number of match entries: ", sentinel2
